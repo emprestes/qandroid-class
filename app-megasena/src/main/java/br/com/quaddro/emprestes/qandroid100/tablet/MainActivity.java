93c178e9af30190a5711ca920de925c01280aace
Prestes, E. M <emprestes@gmail.com>
2016-10-25 23:14:06 +0100

    Aula 11

--- app-megasena/src/main/java/br/com/quaddro/emprestes/qandroid100/tablet/MainActivity.java
+++ app-megasena/src/main/java/br/com/quaddro/emprestes/qandroid100/tablet/MainActivity.java
@@ -1,16 +1,14 @@
 package br.com.quaddro.emprestes.qandroid100.tablet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import br.com.quaddro.emprestes.qandroid100.domain.model.MegaSena;
 import br.com.quaddro.emprestes.qandroid100.tablet.api.OnSeekBarChangeAdapter;
