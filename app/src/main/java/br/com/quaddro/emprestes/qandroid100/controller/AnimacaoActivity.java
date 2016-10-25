09a7cdad85469eae9cf2c9f56f7dea3c94657c0a
Prestes, E. M <emprestes@gmail.com>
2016-10-26 00:35:38 +0100

    Aula 11

--- app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/AnimacaoActivity.java
+++ app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/AnimacaoActivity.java
@@ -2,13 +2,38 @@ package br.com.quaddro.emprestes.qandroid100.controller;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
+import br.com.quaddro.emprestes.qandroid100.R;
 import br.com.quaddro.emprestes.qandroid100.api.QuaddroActivity;
 
 public class AnimacaoActivity extends QuaddroActivity {
 
+    private ImageView iv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.animacao_view);
+
+        iv = getViewById(R.id.iv_rico);
+    }
+
+    private void startAnimation(int id) {
+        Animation a = AnimationUtils.loadAnimation(this, id);
+
+        iv.startAnimation(a);
+    }
+
+    public void rotacionar01(View view) {
+        startAnimation(R.anim.rotacionar_dir);
+    }
+
+    public void rotacionar02(View view) {
+        startAnimation(R.anim.rotacionar_esq);
     }
 }
