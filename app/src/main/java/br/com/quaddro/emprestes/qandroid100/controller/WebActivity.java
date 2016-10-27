38a5bfdaac8aa8b2823c5f6fb31806269ec0ccd7
Prestes, E. M <emprestes@gmail.com>
2016-10-28 00:44:55 +0100

    Aula 13

--- app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/WebActivity.java
+++ app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/WebActivity.java
@@ -2,6 +2,9 @@ package br.com.quaddro.emprestes.qandroid100.controller;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -34,14 +37,14 @@ public class WebActivity extends QuaddroActivity {
 
         go.setOnClickListener(new View.OnClickListener() {
             private final String http = "http://";
-            private final String www = "www.";
+//            private final String www = "www.";
 
             @Override
             public void onClick(View v) {
                 String u;
 
                 u = url.getText().toString();
-                u = u.contains(www)  ? u : www.concat(u);
+//                u = u.contains(www)  ? u : www.concat(u);
                 u = u.contains(http) ? u : http.concat(u);
 
                 content.loadUrl(u);
@@ -50,12 +53,27 @@ public class WebActivity extends QuaddroActivity {
     }
 
     @Override
-    public void onBackPressed() {
-        // Ou vc usa OOP, ou vc usa POG (anti-pattern)
-        if (content.canGoBack()) {
-            content.goBack();
-        } else {
-            super.onBackPressed();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mi = new MenuInflater(this);
+
+        mi.inflate(R.menu.web_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onMenuClicked(MenuItem menu) {
+        switch (menu.getItemId()) {
+            case R.id.web_back:
+                // Ou vc usa OOP, ou vc usa POG (anti-pattern)
+                if (content.canGoBack()) {
+                    content.goBack();
+                }
+                break;
+            case R.id.web_forward:
+                if (content.canGoForward()) {
+                    content.goForward();
+                }
+                break;
         }
     }
 }
