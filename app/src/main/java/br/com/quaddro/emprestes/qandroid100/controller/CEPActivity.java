25ea2d938439833fb712429f1523a72da10867ef
Prestes, E. M <emprestes@gmail.com>
2016-11-10 00:52:32 +0000

    Aula 21

--- app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/CEPActivity.java
+++ app/src/main/java/br/com/quaddro/emprestes/qandroid100/controller/CEPActivity.java
@@ -1,19 +1,15 @@
 package br.com.quaddro.emprestes.qandroid100.controller;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import android.widget.Toast;
 
 import br.com.quaddro.emprestes.qandroid100.R;
 import br.com.quaddro.emprestes.qandroid100.api.QuaddroActivity;
@@ -22,6 +18,24 @@ public class CEPActivity extends QuaddroActivity {
 
     private TextView endereco;
     private EditText cep;
+
+    private BroadcastReceiver enderecoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String e, m;
+
+            e = intent.getStringExtra("endereco");
+
+            if (e.isEmpty()) {
+                m = String.format("%s :(", cep.getText());
+            } else {
+                endereco.setText(e);
+                m = String.format("%s :D", cep.getText());
+            }
+
+            Toast.makeText(getApplicationContext(), m, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,29 +47,24 @@ public class CEPActivity extends QuaddroActivity {
         cep = getViewById(R.id.cep);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        registerReceiver(enderecoReceiver, new IntentFilter(CEPService.ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(enderecoReceiver);
+        super.onPause();
+    }
+
     public void buscar(View view) {
-        String endereco;
-        URL url;
-        URLConnection connection;
+        Intent i = new Intent(this, CEPService.class);
 
-        try {
-            url = new URL(String.format("http://api.postmon.com.br/v1/cep/%s", cep.getText()));
-            connection = url.openConnection();
+        i.putExtra("cep", cep.getText());
 
-            try (InputStream in = connection.getInputStream();
-                 BufferedReader buffer = new BufferedReader(new InputStreamReader(in))) {
-                JSONObject json = new JSONObject(buffer.readLine());
-
-                endereco = String.format("%s, %s, %s, %s",
-                        json.getString("logradouro"),
-                        json.getString("cep"),
-                        json.getString("bairro"),
-                        json.getString("cidade"));
-
-                this.endereco.setText(endereco);
-            }
-        } catch (Exception cause) {
-            Log.e("CEP", "Problemas", cause);
-        }
+        startService(i);
     }
 }
